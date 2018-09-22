96a6b4c8167a7af5f52509983b2f93855ae60b7b
David Berdik <berdikd@duq.edu>
2018-09-22 18:05:29 -0400

    Writing to the database is now handled by one method which will call the other three methods for writing to each table in the appropriate order

--- src/hackathon/sdlc/database/DbConn.java
+++ src/hackathon/sdlc/database/DbConn.java
@@ -24,7 +24,26 @@ public class DbConn {
 		} catch (Exception e) {}
 	}
 	
-	public void writeGenInfo(String nameFirst, String nameLast, String date, String streetAddress, 
+	public boolean writeToDb(String genNameFirst, String genNameLast, String genDate, 
+			String genStreetAddress, String genApartmentNum, String genCity, String genState, 
+			String genZip, String genPhone, String genEmail, String genDob, String genBirthSex, 
+			String genSelfIdSex, String insInsuranceId, String insGroupCode, String insStartDate, 
+			String insEndDate, String insCoverageType, String depNameFirst, String depNameMiddleInitial, 
+			String depNameLast, String depDob, String depBirthSex, String depSelfIdSex, String depPhone, 
+			String depEmail) {
+		// Write the data that has been passed in to the function to the database. Return true if the write
+		// operation completes without exceptions and false if it doesn't.
+		try {
+			writeGenInfo(genNameFirst, genNameLast, genDate, genStreetAddress, genApartmentNum, genCity, 
+					genState, genZip, genPhone, genEmail, genDob, genBirthSex, genSelfIdSex);
+			writeInsuranceInfo(insInsuranceId, insGroupCode, insStartDate, insEndDate, insCoverageType);
+			writeDependentInfo(depNameFirst, depNameMiddleInitial, depNameLast, depDob, depBirthSex, 
+					depSelfIdSex, depPhone, depEmail);
+			return true;
+		} catch (Exception e) {return false;}
+	}
+	
+	private void writeGenInfo(String nameFirst, String nameLast, String date, String streetAddress, 
 			String apartmentNum, String city, String state, String zip, String phone, String email,
 			String dob, String birthSex, String selfIdSex) throws SQLException {
 		// Write to the "General Information" table.
@@ -53,7 +72,7 @@ public class DbConn {
 			id = results.getInt(1);
 	}
 	
-	public void writeInsuranceInfo(String insuranceId, String groupCode, String startDate, 
+	private void writeInsuranceInfo(String insuranceId, String groupCode, String startDate, 
 			String endDate, String coverageType) throws SQLException {
 		// Write to the "Insurance Information" table.
 		PreparedStatement query = dbConn.prepareStatement("INSERT INTO sdlc.insurance VALUES (?, ?, ?, ?, ?, ?)");
@@ -66,7 +85,7 @@ public class DbConn {
 		query.executeUpdate();
 	}
 	
-	public void writeDependentInfo(String nameFirst, String nameMiddleInitial, String nameLast, 
+	private void writeDependentInfo(String nameFirst, String nameMiddleInitial, String nameLast, 
 			String dob, String birthSex, String selfIdSex, String phone, String email) throws SQLException {
 		// Write to the "Dependent Information" table.
 		PreparedStatement query = dbConn.prepareStatement("INSERT INTO sdlc.dependent VALUES (?, ?, ?, ?, ?, "
--- src/hackathon/sdlc/database/DbTest.java
+++ src/hackathon/sdlc/database/DbTest.java
@@ -3,12 +3,7 @@ package hackathon.sdlc.database;
 public class DbTest {
 	public static void main(String[] args) {
 		DbConn conn = new DbConn();
-		try {
-			conn.writeGenInfo("1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13");
-			conn.writeInsuranceInfo("14", "15", "16", "17", "18");
-			conn.writeDependentInfo("19", "20", "21", "22", "23", "24", "25", "26");
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+		conn.writeToDb("1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", 
+				"15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26");
 	}
 }

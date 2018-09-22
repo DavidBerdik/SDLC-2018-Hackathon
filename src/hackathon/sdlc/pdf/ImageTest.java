e9fa968bdf98f0c98d4f1648cee8a179c8bd93c0
Drewerth <natalj1@duq.edu>
2018-09-22 12:28:53 -0400

    Update test casing.

--- src/hackathon/sdlc/pdf/ImageTest.java
+++ src/hackathon/sdlc/pdf/ImageTest.java
@@ -9,31 +9,31 @@ public class ImageTest {
 	// Load files from local system (John Natale) and attempt to properly process them, then save them onto the desktop.
 	public static void start() {
 		// Manually add our files.
-		ConversionUtilities.addFileToQueue(new File("C:\\Users\\Administrator\\Desktop\\Hackathon\\test-data\\01.pdf"));
-		ConversionUtilities.addFileToQueue(new File("C:\\Users\\Administrator\\Desktop\\Hackathon\\test-data\\02.pdf"));
-		ConversionUtilities.addFileToQueue(new File("C:\\Users\\Administrator\\Desktop\\Hackathon\\test-data\\03.pdf"));
-		ConversionUtilities.addFileToQueue(new File("C:\\Users\\Administrator\\Desktop\\Hackathon\\test-data\\04.pdf"));
-		ConversionUtilities.addFileToQueue(new File("C:\\Users\\Administrator\\Desktop\\Hackathon\\test-data\\05.pdf"));
-		ConversionUtilities.addFileToQueue(new File("C:\\Users\\Administrator\\Desktop\\Hackathon\\test-data\\06.pdf"));
-		ConversionUtilities.addFileToQueue(new File("C:\\Users\\Administrator\\Desktop\\Hackathon\\test-data\\07.pdf"));
-		ConversionUtilities.addFileToQueue(new File("C:\\Users\\Administrator\\Desktop\\Hackathon\\test-data\\08.pdf"));
-		ConversionUtilities.addFileToQueue(new File("C:\\Users\\Administrator\\Desktop\\Hackathon\\test-data\\09.pdf"));
-		ConversionUtilities.addFileToQueue(new File("C:\\Users\\Administrator\\Desktop\\Hackathon\\test-data\\10.pdf"));
+		ConversionUtilities.addFileToQueue(new File("01.pdf"));
+		ConversionUtilities.addFileToQueue(new File("02.pdf"));
+		ConversionUtilities.addFileToQueue(new File("03.pdf"));
+		ConversionUtilities.addFileToQueue(new File("04.pdf"));
+		ConversionUtilities.addFileToQueue(new File("05.pdf"));
+		ConversionUtilities.addFileToQueue(new File("06.pdf"));
+		ConversionUtilities.addFileToQueue(new File("07.pdf"));
+		ConversionUtilities.addFileToQueue(new File("08.pdf"));
+		ConversionUtilities.addFileToQueue(new File("09.pdf"));
+		ConversionUtilities.addFileToQueue(new File("10.pdf"));
 		
 		// Convert to BufferedImage list.
 		List<BufferedImage> list = ConversionUtilities.processQueue();
 		
 		// Write to disk.
-		PNGWriter.create("C:\\Users\\Administrator\\Desktop\\01", list.get(0));
-		PNGWriter.create("C:\\Users\\Administrator\\Desktop\\02", list.get(1));
-		PNGWriter.create("C:\\Users\\Administrator\\Desktop\\03", list.get(2));
-		PNGWriter.create("C:\\Users\\Administrator\\Desktop\\04", list.get(3));
-		PNGWriter.create("C:\\Users\\Administrator\\Desktop\\05", list.get(4));
-		PNGWriter.create("C:\\Users\\Administrator\\Desktop\\06", list.get(5));
-		PNGWriter.create("C:\\Users\\Administrator\\Desktop\\07", list.get(6));
-		PNGWriter.create("C:\\Users\\Administrator\\Desktop\\08", list.get(7));
-		PNGWriter.create("C:\\Users\\Administrator\\Desktop\\09", list.get(8));
-		PNGWriter.create("C:\\Users\\Administrator\\Desktop\\10", list.get(9));
+		PNGWriter.create("01"+ "_test", list.get(0));
+		PNGWriter.create("02"+ "_test", list.get(1));
+		PNGWriter.create("03"+ "_test", list.get(2));
+		PNGWriter.create("04"+ "_test", list.get(3));
+		PNGWriter.create("05"+ "_test", list.get(4));
+		PNGWriter.create("06"+ "_test", list.get(5));
+		PNGWriter.create("07"+ "_test", list.get(6));
+		PNGWriter.create("08"+ "_test", list.get(7));
+		PNGWriter.create("09"+ "_test", list.get(8));
+		PNGWriter.create("10"+ "_test", list.get(9));
 	}
 	
 }

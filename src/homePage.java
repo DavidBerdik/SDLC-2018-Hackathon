99b09376f20ee02a372242f1c76f81ee7821f943
Maria Foster <fosterm3@duq.edu>
2018-09-22 11:24:42 -0400

    Added HTML files to the front end resource folders.

--- src/homePage.java
+++ src/homePage.java
@@ -1,11 +1,14 @@
 
 
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
 
 /**
  * Servlet implementation class homePage
@@ -26,7 +29,17 @@ public class homePage extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: YOLOSWAG ").append(request.getContextPath());
+		//response.getWriter().append("Served at: YOLOSWAG ").append(request.getContextPath());
+		
+		// READ IN HTML FILE 
+		String htmlHomePageContent = "";
+		try {
+			
+			BufferedReader input = new BufferedReader ( new FileReader("hackathonProject.html"));
+		} 
+		catch(Exception e){
+			
+		}
 	}
 
 	/**

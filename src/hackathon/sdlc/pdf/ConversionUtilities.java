07c29109a84661d066d9f828075b0830d52da8f6
Drewerth <natalj1@duq.edu>
2018-09-22 11:55:28 -0400

    Add base for ConversionUtilies.addFileToQueue

--- src/hackathon/sdlc/pdf/ConversionUtilities.java
+++ src/hackathon/sdlc/pdf/ConversionUtilities.java
@@ -1,5 +1,41 @@
 package hackathon.sdlc.pdf;
 
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.LinkedList;
+import java.util.List;
+
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.encryption.InvalidPasswordException;
+
 public class ConversionUtilities {
+	
+	private static List<PDDocument> renderQueue = new LinkedList<PDDocument>();
+
+	public static boolean addFileToQueue(File file) {
+		// Confirm that we have a real file before progressing.
+		if (!file.exists()) return false; 
+	
+		// Make an attempt to load the file into a PDF document.
+		PDDocument pdf;
+		try {
+			pdf = PDDocument.load(file);
+		} catch (InvalidPasswordException e) {
+			System.out.println("ConversionUtilities::addFileToQueue -> InvalidPasswordException");
+			return false;
+		} catch (IOException e) {
+			System.out.println("ConversionUtilities::addFileToQueue -> IOException");
+			return false;
+		}
+		
+		// Add our new PDF to the queue.
+		renderQueue.add(pdf);
+		return true;
+	}
+	
+	public static List<BufferedImage> processQueue() {
+		List<BufferedImage> processed = new LinkedList<BufferedImage>();
+	}
 
 }

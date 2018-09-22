ca2d6ab241c625ac45fcb8e8070625e194b83cf4
Drewerth <natalej1@duq.edu>
2018-09-22 18:08:27 -0400

    Add FTPServer.

---
+++ src/hackathon/sdlc/ftp/FTPServer.java
@@ -0,0 +1,36 @@
+package hackathon.sdlc.ftp;
+
+import java.io.IOException;
+import java.net.SocketException;
+
+import org.apache.commons.net.ftp.FTPClient;
+
+public class FTPServer {
+
+	private String host;
+	private int port;
+	private FTPClient ftp;
+	
+	public FTPServer(String h, int p) {
+		this.host = h;
+		this.port = p;
+		
+		ftp = new FTPClient();
+	}
+	
+	public FTPClient connect(FTPClient client) {
+		// Create a new connection to our FTP client and return it.
+		try {
+			ftp.connect(host, port);
+		} catch (SocketException e) {
+			System.out.println("FTPServer::connect -> SocketExcepton");
+			return null;
+		} catch (IOException e) {
+			System.out.println("FTPServer::connect -> IOException");
+			return null;
+		}
+		
+		return ftp;
+	}
+	
+}

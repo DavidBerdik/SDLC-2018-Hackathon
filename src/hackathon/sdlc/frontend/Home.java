5e50c3685443ad9d9ba177a1997b6066bc55a705
Maria Foster <fosterm3@duq.edu>
2018-09-22 21:08:05 -0400

    Update For DAVID

--- src/hackathon/sdlc/frontend/Home.java
+++ src/hackathon/sdlc/frontend/Home.java
@@ -1,15 +1,41 @@
 package hackathon.sdlc.frontend;
+import java.io.*;
+import java.util.*;
 
+
+import java.awt.List;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.File;
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.logging.Logger;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Part;
+
+import org.apache.commons.net.ftp.FTPClient;
+import org.omg.CORBA.portable.InputStream;
+
+import com.mysql.jdbc.Constants;
+
+import hackathon.sdlc.ftp.FTPServer;
+import hackathon.sdlc.ftp.FTPUser;
+import hackathon.sdlc.ftp.FileActions;
+
+
+
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.FileUploadException;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
+import org.apache.commons.io.output.*;
 
 @WebServlet("/homePage")
 public class Home extends HttpServlet {
@@ -20,6 +46,10 @@ public class Home extends HttpServlet {
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
+
+    
+    
+
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// response.getWriter().append("Served at: YOLOSWAG ").append(request.getContextPath());
 		// READ IN HTML FILE 
@@ -30,7 +60,7 @@ public class Home extends HttpServlet {
 			String ln = "";
 			while((ln = input.readLine())!= null) {
 				htmlHomePageContent.append(ln);
-				System.out.println(htmlHomePageContent);
+				//System.out.println(htmlHomePageContent);
 			}
 			input.close();
 		} 
@@ -46,8 +76,16 @@ public class Home extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		doGet(request, response);
+		String [] temp = request.getParameterValues("pdfCollect");
+		for(int i =0; i < temp.length; i++) {
+		System.out.println(temp[i]);
+		}
+		
+
+	
 	}
+		
+	
 	
 	public File getResourceFile(String fileName) {
 		// Returns a file from the project resources based on the path name.
@@ -65,5 +103,11 @@ public class Home extends HttpServlet {
 		} catch (UnsupportedEncodingException e) {}
 		return null;
 	}
+	
+	
+
+	
+	
+
 
 }
